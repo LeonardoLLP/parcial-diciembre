917168eac3711a49600e56c60b92bb26bec2efc4
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-14 11:04:17 +0100

    Endedthe code with the asked print screen

--- the-minion-game.py
+++ the-minion-game.py
@@ -17,9 +17,15 @@ stuart_points = 0
 for vocal in vocals:
     kevin_points += string_to_count.count(vocal) * possible_combinations(len(string_to_count) - 1)
 
-print(kevin_points)
-
 for letter in string_to_count:
     if letter not in vocals:
-        stuart_points += 
+        stuart_points += possible_combinations(len(string_to_count) - 1)
+
+
+if kevin_points > stuart_points:
+    print("Kevin won with {} points".format(kevin_points))
+elif stuart_points > kevin_points:
+    print("Stuart won with {} points".format(stuart_points))
+else:
+    print("It was a draw")
 
