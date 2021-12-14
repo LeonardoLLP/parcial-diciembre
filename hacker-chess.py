778b00afad546b509d76ea89f77f194e8342bfe0
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-14 11:23:38 +0100

    Intial setup of file and started vertical rooks function

---
+++ hacker-chess.py
@@ -0,0 +1,78 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+
+# Complete the 'verticalRooks' function below.
+
+#
+
+# The function is expected to return a STRING.
+
+# The function accepts following parameters:
+
+#  1. INTEGER_ARRAY r1
+
+#  2. INTEGER_ARRAY r2
+
+#
+
+
+
+def verticalRooks(n, r1, r2):
+    # If you think it for a moment, the winner is decided just by the dimensions of the board AND how many rooks are stuck together when it starts the game
+    # The best option is to always stick to the rook
+    positions = []
+    pass
+    for i in n:
+        
+    
+
+
+
+if __name__ == '__main__':
+
+    with open("hacker-chess-results", 'w') as fptr:
+
+        t = int(input().strip())
+        print(t)
+
+
+
+        for t_itr in range(t):
+
+            n = int(input().strip())
+
+            r1 = []
+
+
+
+            for _ in range(n):
+
+                r1_item = int(input().strip())
+
+                r1.append(r1_item)
+
+
+
+            r2 = []
+
+
+
+            for _ in range(n):
+
+                r2_item = int(input().strip())
+
+                r2.append(r2_item)
+
+
+
+            result = verticalRooks(n, r1, r2)
+
+
+            # fptr.write(result + '\n')
