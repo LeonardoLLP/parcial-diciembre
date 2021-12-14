f2e5af36517e837de77348f26e4604fdabff2d84
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-14 10:54:04 +0100

    Speedrun through the code

---
+++ the-minion-game.py
@@ -0,0 +1,19 @@
+import math
+string_to_play = "BANANA"
+
+string_to_count = string_to_play.casefold()
+
+def possible_combinations(string_: str):
+    return math.factorial(string_)
+
+vocals = ["a", "e", "i", "o", "u"]
+
+n_vocals = 0
+n_consonants = 0
+
+kevin_points = 0
+stuart_points = 0
+
+for vocal in vocals:
+    n_vocals += string_to_count.count(vocal)
+n_consona
